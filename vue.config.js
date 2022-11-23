5720d26cfe85fd5388ef64c31f6633bd3e05a3b7
Tlek Suyerbassov <tleksuerbasov@mail.ru>
2022-11-23 21:45:50 +0600

    fix(app): changed the version of package compression

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,31 @@
-const zlib = require("zlib");
+const CompressionPlugin = require('compression-webpack-plugin');
+const zlib = require('zlib');
 
 module.exports = {
     outputDir: "distAviata",
+    chainWebpack(config) {
+        config.plugins.delete('prefetch');
+        config.plugin('CompressionPlugin').use(CompressionPlugin);
+    },
     pluginOptions: {
-    compression:{
-      brotli: {
-        filename: '[file].br[query]',
-        algorithm: 'brotliCompress',
-        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
-        compressionOptions: {
-          params: {
-            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-          },
+        compression: {
+        brotli: {
+            filename: '[file].br[query]',
+            algorithm: 'brotliCompress',
+            include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+            compressionOptions: {
+            params: {
+                [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+            },
+            },
+            minRatio: 0.8,
         },
-        minRatio: 0.8,
-      },
-      gzip: {
-        filename: '[file].gz[query]',
-        algorithm: 'gzip',
-        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
-        minRatio: 0.8,
-      }
-    }
-  }
-} 
+        gzip: {
+            filename: '[file].gz[query]',
+            algorithm: 'gzip',
+            include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+            minRatio: 0.8,
+        },
+        },
+    },
+};
